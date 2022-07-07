ddcef2b91e2e2a71da2561732216dca79a153e1a
SubToIan <108856703+SubToIan@users.noreply.github.com>
2022-07-07 15:53:37 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+a=3
+b=1973
+print(a+b)
+print(a-b)
+print(a*b)
+print (a/b)
+a=7
+b=3
+print(a // b)
+print(a % b)
+print(a**b)
+print(-a)
+a=80
+b=75
+c=65
+print((a+b+c)/3)
+a=80000
+b=3
+print(a/b)
+print(a//b)
+print(a%b)
+a=294723573920
+b=3
+print(a/b)
+print('지나가던 한 사람이 물었다.')
+print('"실례합니다. 여의도역으로 가는 길이 어디입니까?"')
+print("질문받은 사람이 대답했다.")
+print('\"I don\'t know\"')
+a=2022
+b=7
+c=7
+print((a+b)/c)
+print((a/c)+(b/c))
+print((a/c)-(b/c))
+print((a-b)/c)
+
+a=472
+b=385
+c = b % 10 # b의 일의 자리 숫자
+d = (b//10) % 10
+e = (b//100) % 10
+print(a*c)
+print(a*d)
+print(a*e)
+a=10000
+b=25000
+c=30000
+print('평균', (a+b+c)/3)
+print("총", a+b+c)
